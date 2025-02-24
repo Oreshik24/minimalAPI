697ee1ab745af1d823f596306400278e42b40e84
Oreshin Vladislav <oreshin_vlad2004@mail.ru>
2025-02-24 16:40:30 +0500

    Create miniimal Api with start page

--- minimalAPI/obj/Debug/net8.0/minimalAPI.AssemblyInfo.cs
+++ minimalAPI/obj/Debug/net8.0/minimalAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("minimalAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ffb8e934412aea48a12bca094c3094a495e3df7")]
 [assembly: System.Reflection.AssemblyProductAttribute("minimalAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("minimalAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
